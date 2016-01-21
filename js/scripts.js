d815d2de27cab9b924b09ebd4b0ce7d8b20b1fa5
Jess Guilford and Kyle Derrick <jess.guilford@gmail.com>
2016-01-21 09:36:00 -0800

    add initial files and first test

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var numberCounter = function(number1) {
+var numbers= ""
+  for (var i = 1; i <= number1; i += 1) {
+    numbers +=i;
+  };
+  return numbers;
+};
---
+++ specs/specs.js
@@ -0,0 +1,5 @@
+describe("numberCounter", function () {
+  it("counts up to a given number by 1", function() {
+    expect(numberCounter(10)).to.equal("12345678910");
+  });
+});
